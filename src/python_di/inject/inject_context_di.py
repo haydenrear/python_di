9bcf6f08096c0d876847abe9b29f094aa4b72b04
hayden.rear <hayden.rear@gmail.com>
2023-12-16 13:39:38 -0500

    Updated

--- src/python_di/inject/inject_context_di.py
+++ src/python_di/inject/inject_context_di.py
@@ -82,33 +82,43 @@ class InjectionDescriptor:
 def retrieve_descriptor(value: typing.Union[typing.Type, str],
                         key: typing.Optional[str] = None,
                         default_value: typing.Optional = None,
+                        scope_decorator: injector.ScopeDecorator = None,
+                        profile: str = None,
                         injection_descriptor: typing.Optional[InjectionDescriptor] = None,
                         ctx: typing.Optional[InjectionContext] = None):
     if injection_descriptor is None:
         assert isinstance(value, typing.Type)
-        return ctx.get_interface(value, scope=injector.singleton)
+        if scope_decorator is not None:
+            scope_decorator = injector.singleton
+        return ctx.get_interface(value, profile=profile, scope=scope_decorator)
     if injection_descriptor.skip_if_optional and 'Optional' in str(value):
         return None
     if injection_descriptor.injection_ty == InjectionType.Property:
         assert value == str or value == typing.Optional[str]
         assert key is not None
-        assert injection_descriptor is not None
-        return ctx.get_property_with_default(key, default_value,
-                                             injection_descriptor.get_profile(),
+        if injection_descriptor.get_profile() is not None:
+            profile = injection_descriptor.get_profile()
+        return ctx.get_property_with_default(key, default_value, profile,
                                              **(injection_descriptor.dep_kwargs
                                                 if injection_descriptor.dep_kwargs is not None
                                                 else {}))
     else:
         assert injection_descriptor.injection_ty == InjectionType.Dependency
         assert isinstance(value, typing.Type)
-        return ctx.get_interface(value, profile=injection_descriptor.get_profile(),
-                                 scope=injection_descriptor.get_scope())
+        if injection_descriptor.get_profile() is not None:
+            profile = injection_descriptor.get_profile()
+        if injection_descriptor.get_scope() is None:
+            scope_decorator = injection_descriptor.get_scope()
+        return ctx.get_interface(value, profile=profile, scope=scope_decorator)
 
 
-
-def autowire_fn(descr: dict[str, InjectionDescriptor] = None):
+def autowire_fn(descr: dict[str, InjectionDescriptor] = None,
+                scope_decorator: injector.ScopeDecorator = None,
+                profile: str = None):
     """
     Wrap the function that needs values from the context.
+    :param profile:
+    :param scope_decorator:
     :param descr:
     :param ctx:
     :return:
@@ -120,8 +130,7 @@ def autowire_fn(descr: dict[str, InjectionDescriptor] = None):
             get_create_inject_context(fn)
             inject_proxy.wrapped_fn = fn
             args_to_call = {}
-            for i, k_v in enumerate(
-                    python_util.reflection.reflection_utils.get_all_fn_param_types(fn).items()):
+            for i, k_v in enumerate(python_util.reflection.reflection_utils.get_all_fn_param_types(fn).items()):
                 fn_arg_key = k_v[0]
                 ty_default_tuple = k_v[1]
                 ty_value_reflected = ty_default_tuple[0]
@@ -132,8 +141,10 @@ def autowire_fn(descr: dict[str, InjectionDescriptor] = None):
                     args_to_call[fn_arg_key] = kwargs[fn_arg_key]
                 elif ty_value_reflected is not None:
                     args_to_call[fn_arg_key] = retrieve_descriptor(ty_value_reflected, fn_arg_key, default_value,
-                                                          descr[fn_arg_key] if descr is not None and fn_arg_key in descr.keys()
-                                                          else None)
+                                                                   scope_decorator, profile,
+                                                                   descr[fn_arg_key]
+                                                                   if descr is not None and fn_arg_key in descr.keys()
+                                                                   else None)
                 elif default_value is None:
                     LoggerFacade.error("Found autowire fn with arg that has no default value, no value provided, and "
                                        "no type to inject from.")
